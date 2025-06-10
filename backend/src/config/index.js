082a3e2d91d7201d419d28092c9717ede26262e1
zild <bilaldi393@gmail.com>
2025-06-10 10:03:33 +0700

    gnti port

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT,
+  // port: process.env.PORT || 5000,
   mongodbUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sampis',
   jwtSecret: process.env.JWT_SECRET || 'sampiscihuyrawr',
   nodeEnv: process.env.NODE_ENV || 'development'
